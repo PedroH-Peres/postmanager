71f06b03bbc970b8199229132945e48a038fdd03
Pedro Henrique Bohling Peres <pedrohperescode@gmail.com>
2023-12-30 14:43:34 -0300

    BotNavBar

--- postmanager/lib/pages/overview.dart
+++ postmanager/lib/pages/overview.dart
@@ -20,25 +20,88 @@ class _OverviewState extends State<Overview> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Post Manager",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              index = 1;
+            });
+          },
+          elevation: 0,
+          child: Icon(Icons.add, color: Colors.white, size: index == 1 ? 30 : 25,),
+          backgroundColor: Color.fromARGB(255, 235, 170, 248),
         ),
-        backgroundColor: const Color.fromARGB(255, 180, 90, 200),
-        elevation: 6,
-        shadowColor: Colors.black,
-        actions: [
-          IconButton(onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (_)=>Messagepage()));
-          }, icon: Icon(Icons.message, color: Colors.white)),
-          IconButton(onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (_)=>Configpage()));
-          }, icon: Icon(Icons.settings, color: Colors.white)),
-        ],
-      ),
-      body: pages[index],
-      bottomNavigationBar: BottomNavigationBar(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        appBar: AppBar(
+          title: const Text(
+            "Post Manager",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+          ),
+          backgroundColor: const Color.fromARGB(255, 180, 90, 200),
+          elevation: 6,
+          shadowColor: Colors.black,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (_) => Messagepage()));
+                },
+                icon: Icon(Icons.message, color: Colors.white)),
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (_) => Configpage()));
+                },
+                icon: Icon(Icons.settings, color: Colors.white)),
+          ],
+        ),
+        body: pages[index],
+        bottomNavigationBar: BottomAppBar(
+          color: Color.fromARGB(255, 225, 172, 235),
+          notchMargin: 3,
+          elevation: 1,
+          child: Container(
+              child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              
+              IconButton(
+                iconSize: 35,
+                onPressed: () {
+                  setState(() {
+                    index = 0;
+                  });
+                },
+                icon: Icon(index == 0 ?Icons.home :Icons.home_outlined, color: Colors.white,),
+                
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              IconButton(
+                iconSize: 35,
+                onPressed: () {
+                  setState(() {
+                    index = 2;
+                  });
+                },
+                icon: Icon(index == 2 ?Icons.person :Icons.person_outline, color: Colors.white,),
+              )
+            ],
+          )),
+          shape: AutomaticNotchedShape(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(15),
+              ),
+            ),
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+          ),
+        ));
+  }
+}
+/* BottomNavigationBar(
         currentIndex: index,
         onTap: (i){
           setState(() {
@@ -50,7 +113,4 @@ class _OverviewState extends State<Overview> {
           BottomNavigationBarItem(icon: Icon(Icons.add), label: "Add"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
         ],
-      ),
-    );
-  }
-}
+      ), */
